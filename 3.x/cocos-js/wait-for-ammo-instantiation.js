fb04837ae0dd4d6e8d528a322fa21fef35435044
PP_Pro <chinese1236@qq.com>
2022-09-14 16:02:43 +0800

    add 3.x

---
+++ 3.x/cocos-js/wait-for-ammo-instantiation.js
@@ -0,0 +1,13 @@
+System.register(['./instantiated-8d642a00.js'], function (exports) {
+	'use strict';
+	return {
+		setters: [function (module) {
+			exports('default', module.i1);
+		}],
+		execute: function () {
+
+
+
+		}
+	};
+});
